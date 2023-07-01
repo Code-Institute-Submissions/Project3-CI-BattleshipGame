ed0227ff6c9f17d9d5d6bce2d0484b17f88bc5ad
AEmin96 <>
2023-07-01 09:42:33 +0000

    Start game method

--- run.py
+++ run.py
@@ -81,3 +81,45 @@ def get_valid_input():
                 print("Invalid input! Please enter values within the range (0-4).")
         except ValueError:
             print("Invalid input! Please enter valid numbers for row and column.")
+
+def play_game():
+    """
+    Starts the game
+    """
+    print("Welcome to Battleship!")
+    player_name = input("Please enter your name: ")
+
+    """ 
+    Create player and computer objects 
+    """
+
+    player = Player(player_name)
+    computer = Computer()
+
+    """
+    Set up player's ships
+    """
+
+    print("\nSet up your ships.")
+    for _ in range(player.board.num_ships):
+        print("Enter the coordinates for ship {}".format(_ + 1))
+        x, y = get_valid_input()
+        player.board.add_ship(x, y)
+
+    """
+    Set up computers's ships
+    """
+    print("\nSetting up computer's ships.")
+    for _ in range(computer.board.num_ships):
+        x = random_point(computer.board.size)
+        y = random_point(computer.board.size)
+        computer.board.add_ship(x, y)
+
+    
+    game_over = False
+    while not game_over:
+        print("\nPlayer's turn")
+        player.board.print()
+        print("Make a guess.")
+        x, y = get_valid_input()
+        result = player.board.guess
