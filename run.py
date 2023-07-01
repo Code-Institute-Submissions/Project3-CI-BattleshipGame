086ac0be591c00fab872af94b92dabc338439913
AEmin96 <>
2023-07-01 09:30:21 +0000

    Add ship method

--- run.py
+++ run.py
@@ -18,3 +18,23 @@ class Board:
     def print(self):
         for row in self.board:
             print(" ".join(row))
+    
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = "X"
+
+        if (x,y) in self.ships:
+            self.board[x][y] = "*"
+            return "Hit"
+        else:
+            return "Missed"
+    
+    def add_ship(self, x, y, type="computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: You cannot add anymore ships!")
+        else:
+            self.ships.append((x, y))
+            if self.type == "player":
+                self.board[x][y] = "@"
+                
+        
